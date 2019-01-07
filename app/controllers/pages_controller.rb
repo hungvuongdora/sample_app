3a1cb5bf0ef03d7930b2b7b91799a4401a994628
hungvuongdora <hungvuongdora@gmail.com>
2019-01-07 08:04:40 +0700

    Chapter 3: Mostly static pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,8 +8,6 @@ class PagesController < ApplicationController
   private
 
   def set_locale
-    locale = params[:locale].to_s.strip.to_sym
-    I18n.locale = I18n.available_locales.include?(locale) ?
-      locale : I18n.default_locale
+    I18n.locale = params[:locale] || I18n.default_locale
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,6 @@
 require_relative 'boot'
 
 require "rails"
-# Pick the frameworks you want:
 require "active_model/railtie"
 require "active_job/railtie"
 require "active_record/railtie"
@@ -11,20 +10,12 @@ require "action_mailer/railtie"
 require "action_view/railtie"
 require "action_cable/engine"
 require "sprockets/railtie"
-# require "rails/test_unit/railtie"
-
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module SampleApp
   class Application < Rails::Application
     config.load_defaults 5.2
-
-    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
-    config.i18n.available_locales = [:en, :vi]
     config.i18n.default_locale = :vi
-
     config.generators.system_tests = nil
   end
 end
