2fcd0b88f746ffc197f80a6f8eac41f6b2be3908
hungvuongdora <hungvuongdora@gmail.com>
2019-01-07 08:04:40 +0700

    Chapter 3: Mostly static pages

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,15 @@
+class PagesController < ApplicationController
+  before_action :set_locale
+
+  def home; end
+
+  def help; end
+
+  private
+
+  def set_locale
+    locale = params[:locale].to_s.strip.to_sym
+    I18n.locale = I18n.available_locales.include?(locale) ?
+      locale : I18n.default_locale
+  end
+end
