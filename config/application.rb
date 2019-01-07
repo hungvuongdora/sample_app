98ebee35e31cb781da79631a67068620a762ada5
hungvuongdora <hungvuongdora@gmail.com>
2019-01-07 08:04:40 +0700

    Chapter 4: Rails-flavored Ruby

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ Bundler.require(*Rails.groups)
 module SampleApp
   class Application < Rails::Application
     config.load_defaults 5.2
-    config.i18n.default_locale = :vi
-    config.generators.system_tests = nil
-  end
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+    config.i18n.available_locales = [:en, :vi]
+    config.i18n.default_locale = :en
 end
